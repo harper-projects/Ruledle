fba9ffca14fe764105d6db4e919afd0f048d1486
Harper-dev <harpertodd333@gmail.com>
2025-06-07 12:09:44 -0400

    Update rules.js

--- rules.js
+++ rules.js
@@ -10,5 +10,13 @@ const RULES = [
   {
     description: "Start every sentence with the letter T",
     check: (text) => text.trim().toLowerCase().startsWith("t")
+  },
+  {
+    description: "Only use words with 4 or more letters",
+    check: (text) => text.split(" ").every(word => word.length >= 4)
+  },
+  {
+    description: "Only one word allowed",
+    check: (text) => text.trim().split(" ").length === 1
   }
 ];
